f5b8c65dde56e1381ee5d8530883c20077379775
anyamittal08 <anyamittal08@gmail.com>
2023-09-01 16:44:47 +0530

    started making UI fixes

--- src/views/FollowersPage/index.js
+++ src/views/FollowersPage/index.js
@@ -1,9 +1,10 @@
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useParams, Link as ReactRouterLink } from 'react-router-dom';
 import { useQuery } from 'react-query';
 
 import config from '../../config';
 import UserList from '../UserListA';
+import { Divider, List, ListItem, Typography, Tab } from '@mui/material';
 
 const FollowersPage = () => {
     const { username } = useParams();
@@ -28,9 +29,65 @@ const FollowersPage = () => {
         }
     );
 
-    console.log(followerData);
-
-    return <UserList usersArr={followerData} />;
+    return (
+        <>
+            <List sx={{ paddingTop: '10px' }}>
+                <ListItem
+                    divider={true}
+                    sx={{
+                        paddingTop: '0px',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'flex-start',
+                        paddingBottom: '0px',
+                    }}
+                >
+                    <Typography variant="h6">{`${userData?.displayName}`}</Typography>{' '}
+                    <Typography
+                        sx={{
+                            margin: '0px',
+                            color: '#657786',
+                            fontSize: '0.9em',
+                        }}
+                    >{`@${userData.username}`}</Typography>
+                    <div
+                        style={{
+                            display: 'flex',
+                            justifyContent: 'space-around',
+                            color: '#657786',
+                            width: '100%',
+                            margin: '0px',
+                            padding: '0px',
+                        }}
+                    >
+                        <Tab
+                            label="followers"
+                            component={ReactRouterLink}
+                            to={`/${username}/followers`}
+                            sx={{
+                                color: '#657786',
+                                '&:hover': {
+                                    backgroundColor: '#E1E8ED',
+                                },
+                            }}
+                        />
+                        <Tab
+                            label="following"
+                            component={ReactRouterLink}
+                            to={`/${username}/following`}
+                            sx={{
+                                color: '#657786',
+                                '&:hover': {
+                                    backgroundColor: '#E1E8ED',
+                                },
+                            }}
+                        />
+                    </div>
+                </ListItem>
+            </List>
+            <UserList usersArr={followerData} />
+        </>
+    );
 };
 
 export default FollowersPage;
