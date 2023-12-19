3bb1ccec1547119ac6efbc59cd67dc36e06f106f
anyamittal08 <anyamittal08@gmail.com>
2023-12-19 10:31:56 -0500

    added loading and error states to suggested users page

--- src/views/SuggestedUsersPage/index.js
+++ src/views/SuggestedUsersPage/index.js
@@ -2,16 +2,22 @@ import axios from 'axios';
 import { useContext } from 'react';
 import { useQuery } from 'react-query';
 
+import { List, ListItem, Typography } from '@mui/material';
+
 import config from '../../config';
 import { UserContext } from '../../contexts/auth';
 
 import UserList from '../UserListA';
-import { List, ListItem, Typography } from '@mui/material';
+import { LoadingState, ErrorState } from '../../components/StatusComponents';
 
 const SuggestedUsers = () => {
     const auth = useContext(UserContext);
 
-    const { data: suggestedUsers } = useQuery('suggested', async () => {
+    const {
+        isLoading,
+        error,
+        data: suggestedUsers,
+    } = useQuery('suggested', async () => {
         const res = await axios.get(`${config.api}/users/suggested/users`, {
             headers: { authorization: `Bearer ${auth.token}` },
         });
@@ -23,7 +29,13 @@ const SuggestedUsers = () => {
             <ListItem divider={true} sx={{ paddingTop: '0px' }}>
                 <Typography variant="h6">Suggested for you</Typography>
             </ListItem>
-            <UserList usersArr={suggestedUsers} />
+            {isLoading ? (
+                <LoadingState />
+            ) : error ? (
+                <ErrorState />
+            ) : (
+                <UserList usersArr={suggestedUsers} />
+            )}
         </List>
     );
 };
